2c92db40badad45eaea429b5e8124ecf516e716f
Edda Pétursdóttir <eddapetursdottir@gmail.com>
2018-10-31 22:04:22 +0000

    Testing phase

--- app.js
+++ app.js
@@ -1,156 +1,171 @@
 var Twitter = require('twitter');
 var config = require('./config.js');
 var fact = require('./factory.js');
-var Buffer = require('buffer').Buffer;
+// var Buffer = require('buffer').Buffer;
 var Iconv  = require('iconv').Iconv;
 var iconv = new Iconv('ISO-8859-1', 'UTF-8');   // from UFT-8 to ISO
-
 var T = new Twitter(config);
-var parsedTweet;
-var userIds; // all users to follow
-var userId = '3302852710';
 
-// var now = new Date();
-// var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
-// if (millisTill10 < 0) {
-//     millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
-// }
-
-
-
-// Tweet once a day
-// setTimeout(function() {
-//         // Tweet random bullshit
-//         var randStat = fact.getRandPhrase();
-//         T.post('statuses/update', { status: randStat })
-//          // restart connection here with new userlist
-//     }, millisTill10);
-
-
-
-// Get all followers
-getFollowers()
-    .then(startStream)
-    .catch(function(err){
-        console.log('Any error handling here bla: ', err);
-    });
-
-// function handleError(err){
-//     console.log('Handling error?', err);
-// }
-
-var params = {
-    q: '#nodejs',
-    count: 10,
-    result_type: 'recent'
-    // lang: 'en'
-};
-
-// T.get('search/tweets', params, function(err, data, response) {
-//     if(!err){
-//         // console.log('Data: ', data);
-//         // This is where the magic will happen
-//     } else {
-//         console.log(err);
-//     }
-// });
-
-
-
-function getFollowers(){
-    var promise = new Promise(function (resolve, reject) {
-        var params = {
-            screen_name: 'eddapeturs',
-            stringify_ids: true
-        };
-
-        // T.get('followers/ids', params, function (err, data) {
-        //     console.log('data', data);
-        //     ids = data.ids;
-        //     resolve(ids)
-        //     // if(err){
-        //         reject(err);
-        //     // }
-        // });
-
-        var ids = fact.getIds();
-        console.log('ids: ', ids);
-        if(ids){ resolve(ids); }
-        else if(!ids)
-            { reject(); }
-
-    });
-    return promise;
-}
+// startStream();
 
 // Start streaming content from all user following bot
-function startStream(userIds){
+function startStream(){
     var params = {
-        follow: userIds.toString()
+        follow: '1056869573008506880'
     };
 
     var stream = T.stream('statuses/filter', params);
 
     stream.on('data', function (tweet) {
-        if (userIds.indexOf(tweet.user.id_str) > -1) {
-            console.log('INCOMING: ', tweet.user.name + ": " + tweet.text)
-            // var stripped = stripCommand(tweet.text);
-            getProcessedString(stripCommand(tweet));
-            // getParsedString(stripped)
+
+        if(tweet.in_reply_to_user_id_str == '1056869573008506880'){
+            console.log('In reply to: ', tweet.in_reply_to_user_id_str);
+            getProcessedString(stripCommand(tweet))
+                .then(createResponse)
+                .then(updateStatus)
+                .catch(function(err){
+                    console.log('Error: ', err);
+                });
         }
     });
 }
 
-// Update status
-function updateStatus(obj){
-    console.log('@' + obj.username + ' ' + obj.response)
-    var params = {
-        status: '@' + obj.username + ' ' + obj.response
-    };
-    T.post('statuses/update', params);
-}
-
-
 // Parse the string using IceNLP
 function getProcessedString(object){
-    fact.getParsedString(object.text)
+    return fact.getParsedString(object.text)
         .then(function(data){
                 var newStr = data.toString();
                 var jsonStr = JSON.parse(newStr);
-                console.log('JsonTagg: ', jsonStr);
-                object.response = jsonStr[object.type];
-                updateStatus(object);
-                return jsonStr;
+                object.jsonString = jsonStr;
+                return object;
             },
             function(err){
                 console.log('Error: ', err);
             })
 }
 
+// Update status
+function updateStatus(tweetArray){
+    for(var i in tweetArray){
+        console.log('TA: ', tweetArray[i]);
+        var params = {
+            status:  tweetArray[i]
+        };
+        T.post('statuses/update', params);
+    }
+}
+
+
+function createResponse(obj){
+    var promise = new Promise(function (resolve, reject) {
+        var username = obj.username;
+        var taggedStr;
+        var parsedStr;
+        var objArr = [];
+        if(obj.type == 'tp'){
+            taggedStr = obj.jsonString ['tagg'];
+            parsedStr = obj.jsonString ['parse'];
+        }else if(obj.type == 'tagg'){
+            taggedStr = obj.jsonString ['tagg'];
+        } else if(obj.type == 'parse'){
+            parsedStr = obj.jsonString ['parse'];
+        } else if (obj.type == 'noTag'){
+            objArr.push(username + 'skipun vantar, vinsamlegast bættu við -t eða -p.')
+        }
+
+
+        if(parsedStr && parsedStr.length > 280  - username.length){
+            var splitArray = splitHelper(parsedStr);
+            for(var i in splitArray){
+                objArr.push(splitArray[i]);
+            }
+        } else if( parsedStr ) {
+            objArr.push(username + parsedStr);
+        }
+
+        if(taggedStr && taggedStr.length > 280 - username.length){
+            var splitArray = splitHelper(taggedStr);
+            for(var i in splitArray){
+                objArr.push(splitArray[i]);
+            }
+        }
+        else if( taggedStr ) {
+            objArr.push(username + taggedStr);
+        }
+
+        if( objArr ){
+            resolve(objArr);
+        } else {
+            reject();
+        }
+
+
+        function splitHelper(string){
+            var splitArr = [];
+            var max = 280 - username.length;
+
+            var count = max;
+            var length = string.length;
+
+            while(count < (length + max)){
+                var tweet = string.substring(count-max, count);
+                splitArr.push(username + tweet);
+                count += max;
+            }
+            return splitArr;
+        }
+    });
+    return promise;
+}
+
+
+
+
 // Function to remove 'tag' or 'parse' commands from tweet and save it
 function stripCommand(tweet){
-    var str;
     var obj = {
-        username: tweet.user.name,
+        username: '@' + tweet.user.screen_name + ' ',
         text: '',
         type: ''
     };
 
     var tw = tweet.text.toLowerCase();
-    if(tw.includes('-t')){
-        str = tw.replace('-t ', '');        // Replace '-t' with nothing
-        obj.text = str;
-        obj.type = 'tagg'                   // Give correct type for return
+    tw = tw.replace('@malfridurbot', '');
+    if(tw.includes('-t') && tw.includes('-p')){
+        var tmpStr;
+        tmpStr = tw.replace('-t', '');
+        obj.text = tmpStr.replace('-p', '');
+        obj.type = 'tp';
+
+    }
+    else if(tw.includes('-t')){
+        obj.text = tw.replace('-t', '');        // Replace '-t' with nothing
+        obj.type = 'tagg'                        // Give correct type for return
     }
     else if(tw.includes('-p')){
-        str = tw.replace('-p ', '');
-        obj.text = str;
+        obj.text = tw.replace('-p', '');
         obj.type = 'parse';
+    } else {
+        obj.text = tw;
+        obj.type = 'noTag';
     }
     return obj;
 }
 
 
+var now = new Date();
+var millisTill10 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 10, 0, 0, 0) - now;
+if (millisTill10 < 0) {
+    millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+}
+
+// Tweet once a day
+setTimeout(function() {
+    // Tweet random bullshit
+    var randStat = fact.getRandPhrase();
+    T.post('statuses/update', { status: randStat })
+    // restart connection here with new userlist
+}, millisTill10);
 
 
 // testFactory();
@@ -168,3 +183,20 @@ function stripCommand(tweet){
 //             console.log('errTweet: ', err);
 //         })
 // }
+
+testCode();
+//
+function testCode(){
+    var tweet = {
+        text: "@MalfridurBot gamla gamla það þó ekki hafa verið staðfest -t",
+        user: {
+            name: "botthildur"
+        }
+    };
+    getProcessedString(stripCommand(tweet))
+        .then(createResponse)
+        // .then(updateStatus)
+        .catch(function(err){
+            console.log('Error: ', err);
+        });
+}
